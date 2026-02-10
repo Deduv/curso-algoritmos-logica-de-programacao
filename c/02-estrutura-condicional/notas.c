94acc60ab16d25240e7727d93e19bcf6adb89869
Deduv <edusant999@gmail.com>
2026-02-10 11:27:09 -0300

    feat: estudo de estrutura condicional em c 🔀

--- c/02-estrutura-condicional/notas.c
+++ c/02-estrutura-condicional/notas.c
@@ -4,7 +4,23 @@ uma disciplina anual. Em seguida, mostrar a nota final que o aluno obteve (com u
 ano juntamente com um texto explicativo. Caso a nota final do aluno seja inferior a 60.00, mostrar a
 mensagem "REPROVADO", conforme exemplos.*/
 
+
 #include <stdio.h>
 int main () {
+    double n1, n2, fg;
+    
+    printf("Digite a primeira nota: ");
+    scanf("%lf", &n1);
+    printf("Digite a segunda nota: ");
+    scanf("%lf", &n2);
+
+    fg = n1 + n2;
+
+    printf("NOTA FINAL = %.1lf", fg);
+    if (fg >= 60) {
+        printf("\nAPROVADO\n");
+    } else {
+        printf("\nREPROVADO\n");
+    }
     return 0;
 }
