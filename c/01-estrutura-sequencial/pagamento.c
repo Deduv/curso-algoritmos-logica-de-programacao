f63d2c134f6278325a7b8ee9e61977c4b98f9b44
Deduv <edusant999@gmail.com>
2026-02-11 20:22:54 -0300

    feat: estudo de estrutura sequencial em c 🔢

--- c/01-estrutura-sequencial/pagamento.c
+++ c/01-estrutura-sequencial/pagamento.c
@@ -4,13 +4,25 @@ quantidade de horas trabalhadas por ele(a). Ao final, mostrar o valor do pagamen
 uma mensagem explicativa, conforme exemplo.*/
 
 #include <stdio.h> 
+#include <string.h>
+
+void read_text(char * buffer, int lenght) {
+    fgets(buffer, lenght, stdin);
+    strtok(buffer, "\n");
+}
+
+void input_cleanup () {
+    char c;
+    while ((c = getchar()) != '\n' && c != EOF) { }
+}
+
 int main () {
     char name[50];
     double valueperhour, payement;
     int workedhours;
 
     printf("NOME: ");
-    scanf("%s", name);
+    read_text(name, 50);
     printf("VALOR POR HORA: ");
     scanf("%lf", &valueperhour);
     printf("HORAS TRABALHADAS: ");
