f9a9d0a763803cbf155366684f7eeaa796cd9caa
Deduv <edusant999@gmail.com>
2026-02-19 15:56:46 -0300

    feat: estudo de matrizes em c 📊

---
+++ c/05-matrizes/negativos_matriz.c
@@ -0,0 +1,32 @@
+/*Problema "negativos_matriz"
+Ler dois números M e N (máximo = 10), e depois ler uma matriz MxN de números inteiros, conforme
+exemplo. Em seguida, mostrar na tela somente os números negativos da matriz.*/
+
+#include <stdio.h> 
+int main () {
+    int m, n, matriz[10][10];
+    
+    printf("Qual a quantidade de linhas da matriz? ");
+    scanf("%d", &m);
+    printf("Qual a quantidade de colunas da matriz? ");
+    scanf("%d", &n);
+    
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+        printf("Elemento [%d, %d]: ", i, j);
+        scanf("%d", &matriz[i][j]);
+        }
+    }
+
+    printf("VALORES NEGATIVOS: \n");
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            if(matriz[i][j] < 0) {
+            printf("%d\n", matriz[i][j]);
+            }
+        }
+    }
+    
+    
+    return 0;
+}
