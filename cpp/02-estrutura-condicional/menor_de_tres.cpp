e0eb8b7301ef2a874964ef86d993e4c6b2e21c1b
Deduv <edusant999@gmail.com>
2026-02-20 16:58:17 -0300

    feat: estudo de estrutura condicional em cpp 🔀

---
+++ cpp/02-estrutura-condicional/menor_de_tres.cpp
@@ -0,0 +1,31 @@
+/*Problema "menor_de_tres"
+Fazer um programa para ler três números inteiros. Em seguida, mostrar qual o menor dentre os três
+números lidos. Em caso de empate, mostrar apenas uma vez.*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    cout << "Primeiro valor: ";
+    cin >> a;
+    
+    cout << "Segundo valor: ";
+    cin >> b;
+    
+    cout << "Terceiro valor: ";
+    cin >> c;
+
+    // A sua lógica de comparação foi mantida pois está correta
+    if (a <= b && a <= c) {
+        cout << "MENOR = " << a << endl;
+    } else if (b <= c) {
+        cout << "MENOR = " << b << endl;
+    } else {
+        cout << "MENOR = " << c << endl;
+    }
+
+    return 0;
+}
