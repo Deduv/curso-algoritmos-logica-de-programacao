696be9eb5caf8312f5b3193bfb4280afafadf8cf
Deduv <edusant999@gmail.com>
2026-02-23 22:48:05 -0300

    feat: estudo de estrutura sequencial em java

--- java/01-estrutura-sequencial/Idades.java
+++ java/01-estrutura-sequencial/Idades.java
@@ -1,3 +1,6 @@
+/*Problema "idades"
+Fazer um programa para ler o nome e idade de duas pessoas. Ao final mostrar uma mensagem com os
+nomes e a idade média entre essas pessoas, com uma casa decimal, conforme exemplo.*/
 
 import java.util.Locale;
 import java.util.Scanner;
